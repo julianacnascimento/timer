563d1ff4aea5d56e988cefdee93324441647a4f9
Juliana Nascimento <juliana@MacBook-Air-de-Juliana.local>
2025-02-15 18:21:54 -0300

    Create home and history page, components (countdown/new cycle form) and create context

---
+++ src/contexts/CycleContext.tsx
@@ -0,0 +1,12 @@
+import { createContext } from 'react'
+import { Cycle } from '../pages/Home'
+
+interface CycleContextType {
+  activeCycle: Cycle | undefined
+  activeCycleId: string | null
+  amountSecondsPassed: number
+  markCurrentCycleAsFinished: () => void
+  setSecondsPassed: (seconds: number) => void
+}
+
+export const CyclesContext = createContext({} as CycleContextType)
